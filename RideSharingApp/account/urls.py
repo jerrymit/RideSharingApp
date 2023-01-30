bfeccc487fc996e503b61221b452442aee0c915a
James Huang <sh694@duke.edu>
2023-01-30 18:13:47 -0500

    moved

--- RideSharingApp/account/urls.py
+++ RideSharingApp/account/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
 
-from .views import SignUpView
+from .views import *
+from . import views
 
 
 urlpatterns = [
     path("signup/", SignUpView.as_view(), name="signup"),
+    path("driver_info/", views.DriverRegister, name="DriverRegister"),
+    path("driver_info/driver_page/", views.DriverPage, name="DriverPage"),
+    path('rideRequest/', views.RideRequest, name='RideRequest'),
+    path('shareride/', views.Ridesharer, name='Ridesharer'),
+    path('owner/', views.Owner, name='Owner'),
 ]
--- RideSharingApp/account/views.py
+++ RideSharingApp/account/views.py
@@ -4,9 +4,31 @@ from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from django.shortcuts import render, redirect
+
+# Create your views here.
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.models import User, auth 
+from django.urls import reverse
+from django.contrib import messages
 
 
 class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+def DriverRegister(request):
+    return render(request, "registration/driver_info.html")
+
+def DriverPage(request):
+    return render(request, "registration/driver_page.html")
+
+def RideRequest(request):
+    return render(request, "registration/ride_request.html")
+
+def Owner(request):
+    return render(request, "registration/owner_page.html")
+
+def Ridesharer(request):
+    return render(request, "registration/rideshare_page.html")
